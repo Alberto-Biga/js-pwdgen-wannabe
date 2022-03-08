16beadce442bf96a78d0666d7310f4dcb3298de9
AlbertoBiga <alberto.biga10@gmail.com>
2022-03-08 15:30:18 +0100

    Consegna

---
+++ asset/js/main.js
@@ -0,0 +1,12 @@
+const name = prompt ("Ciao! Come ti chiami?");
+
+const surname = prompt ("Qual è il tuo cognome?");
+
+const colour = prompt ("Per favore, confidami ancora qual è il tuo colore preferito..");
+
+const txt = "La tua nuova password é :"
+const number = "21"
+
+
+
+document.getElementById("target").innerHTML =  txt + name + surname + colour+number
