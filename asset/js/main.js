b33cfac8c2f2befd116f4b9bdf8edfbc54b6887d
AlbertoBiga <alberto.biga10@gmail.com>
2022-03-09 10:14:10 +0100

    fine

--- asset/js/main.js
+++ asset/js/main.js
@@ -4,10 +4,10 @@ console.log(name)
 const surname = prompt ("Qual è il tuo cognome?");
 console.log(surname)
 
-const colour = prompt ("Per favore, confidami ancora qual è il tuo colore preferito..");
-console.log(colour)
+const color = prompt ("Per favore, confidami ancora qual è il tuo colore preferito..");
+console.log(color)
 
-const password = name + surname + colour;
+const password = name + surname + color;
 console.log(password)
 
 document.getElementById("target").innerHTML =  "La tua nuova password è : " + password + 21;
